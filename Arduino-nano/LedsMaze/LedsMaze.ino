2a703c21ec5d53b1070a7239443b331e9dc8ca33
jebissey <je.bissey@gmail.com>
2022-05-30 15:25:08 +0200

    Added LedsMaze

---
+++ Arduino-nano/LedsMaze/LedsMaze.ino
@@ -0,0 +1,270 @@
+
+#include<Wire.h>
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+#include <LedControl.h>
+enum Leds8x8 
+{
+  leds_DataIn = 13,
+  leds_CS = 17,
+  led_CLK = 16,
+};
+LedControl lc=LedControl(leds_DataIn,led_CLK,leds_CS);
+
+
+struct Coordonate{
+  int row;
+  int col;
+
+  Coordonate(int row_, int col_)  { row = row_; col = col_;}
+};
+
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime1=500;
+unsigned long delaytime2=50;
+void setup() {
+  /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,8);
+  /* and clear the display */
+  lc.clearDisplay(0);
+
+
+
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+}
+
+/*
+ This method will display the characters for the
+ word "Arduino" one after the other on the matrix. 
+ (you need at least 5x7 leds to see the whole chars)
+ */
+void writeArduinoOnMatrix() {
+  /* here is the data for the characters */
+  byte a[5]={B01111110,B10001000,B10001000,B10001000,B01111110};
+  byte r[5]={B00010000,B00100000,B00100000,B00010000,B00111110};
+  byte d[5]={B11111110,B00010010,B00100010,B00100010,B00011100};
+  byte u[5]={B00111110,B00000100,B00000010,B00000010,B00111100};
+  byte i[5]={B00000000,B00000010,B10111110,B00100010,B00000000};
+  byte n[5]={B00011110,B00100000,B00100000,B00010000,B00111110};
+  byte o[5]={B00011100,B00100010,B00100010,B00100010,B00011100};
+
+  /* now display them one by one with a small delay */
+  lc.setRow(0,0,a[0]);
+  lc.setRow(0,1,a[1]);
+  lc.setRow(0,2,a[2]);
+  lc.setRow(0,3,a[3]);
+  lc.setRow(0,4,a[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,r[0]);
+  lc.setRow(0,1,r[1]);
+  lc.setRow(0,2,r[2]);
+  lc.setRow(0,3,r[3]);
+  lc.setRow(0,4,r[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,d[0]);
+  lc.setRow(0,1,d[1]);
+  lc.setRow(0,2,d[2]);
+  lc.setRow(0,3,d[3]);
+  lc.setRow(0,4,d[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,u[0]);
+  lc.setRow(0,1,u[1]);
+  lc.setRow(0,2,u[2]);
+  lc.setRow(0,3,u[3]);
+  lc.setRow(0,4,u[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,i[0]);
+  lc.setRow(0,1,i[1]);
+  lc.setRow(0,2,i[2]);
+  lc.setRow(0,3,i[3]);
+  lc.setRow(0,4,i[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,n[0]);
+  lc.setRow(0,1,n[1]);
+  lc.setRow(0,2,n[2]);
+  lc.setRow(0,3,n[3]);
+  lc.setRow(0,4,n[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,o[0]);
+  lc.setRow(0,1,o[1]);
+  lc.setRow(0,2,o[2]);
+  lc.setRow(0,3,o[3]);
+  lc.setRow(0,4,o[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,0);
+  lc.setRow(0,1,0);
+  lc.setRow(0,2,0);
+  lc.setRow(0,3,0);
+  lc.setRow(0,4,0);
+  delay(delaytime1);
+}
+
+/*
+  This function lights up a some Leds in a row.
+ The pattern will be repeated on every row.
+ The pattern will blink along with the row-number.
+ row number 4 (index==3) will blink 4 times etc.
+ */
+void rows() {
+  for(int row=0;row<8;row++) {
+    delay(delaytime2);
+    lc.setRow(0,row,B10100000);
+    delay(delaytime2);
+    lc.setRow(0,row,(byte)0);
+    for(int i=0;i<row;i++) {
+      delay(delaytime2);
+      lc.setRow(0,row,B10100000);
+      delay(delaytime2);
+      lc.setRow(0,row,(byte)0);
+    }
+  }
+}
+
+/*
+  This function lights up a some Leds in a column.
+ The pattern will be repeated on every column.
+ The pattern will blink along with the column-number.
+ column number 4 (index==3) will blink 4 times etc.
+ */
+void columns() {
+  for(int col=0;col<8;col++) {
+    delay(delaytime2);
+    lc.setColumn(0,col,B10100000);
+    delay(delaytime2);
+    lc.setColumn(0,col,(byte)0);
+    for(int i=0;i<col;i++) {
+      delay(delaytime2);
+      lc.setColumn(0,col,B10100000);
+      delay(delaytime2);
+      lc.setColumn(0,col,(byte)0);
+    }
+  }
+}
+
+/* 
+ This function will light up every Led on the matrix.
+ The led will blink along with the row-number.
+ row number 4 (index==3) will blink 4 times etc.
+ */
+void single() {
+  for(int row=0;row<8;row++) {
+    for(int col=0;col<8;col++) {
+      delay(delaytime2);
+      lc.setLed(0,row,col,true);
+      delay(delaytime2);
+      for(int i=0;i<col;i++) {
+        lc.setLed(0,row,col,false);
+        delay(delaytime2);
+        lc.setLed(0,row,col,true);
+        delay(delaytime2);
+      }
+    }
+  }
+}
+
+
+
+
+void AllOn(unsigned long delaytime = 0) {
+  for(int row=0;row<8;row++) {
+    for(int col=0;col<8;col++) {
+      lc.setLed(0,row,col,true);
+      delay(delaytime);
+    }
+  }
+}
+
+void AllOff(unsigned long delaytime = 0) {
+  for(int row=0;row<8;row++) {
+    for(int col=0;col<8;col++) {
+      lc.setLed(0,row,col,false);
+      delay(delaytime);
+    }
+  }
+}
+
+void blink(unsigned long delayOn, unsigned long delayOff){
+  AllOn();
+  delay(delayOn);
+  AllOff();
+  delay(delayOff);
+}
+
+void MoveSquare2x2(unsigned long delayOnOf){
+  int lastrow = -1;
+  int last;
+  for(int row = 0; row < 7; row++){
+    for(int col = 0; col < 7; col++){
+      Square2x2(Coordonate(row, col), true);
+      delay(delayOnOf);
+      Square2x2(Coordonate(row, col), false);
+      //delay(delayOnOf);
+    }
+  }
+}
+
+
+
+void Square2x2(Coordonate coordonate, bool state) {
+  lc.setLed(0, coordonate.row, coordonate.col, state);
+  lc.setLed(0, coordonate.row + 1, coordonate.col, state);
+  lc.setLed(0, coordonate.row, coordonate.col + 1, state);
+  lc.setLed(0, coordonate.row + 1, coordonate.col + 1, state);
+}
+
+
+void loop() { 
+  //writeArduinoOnMatrix();
+  //rows();
+  //columns();
+  //single();
+  
+  /*singleOn(50);
+  delay(500);
+  singleOff(25);
+  delay(2000);*/
+  blink(50, 950);
+
+  MoveSquare2x2(100);
+
+
+
+
+
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+  
+  Serial.print("Accelerometer: ");
+  Serial.print("X = "); Serial.print(AcX);
+  Serial.print(" | Y = "); Serial.print(AcY);
+  Serial.print(" | Z = "); Serial.println(AcZ); 
+  
+  Serial.print("Gyroscope: ");
+  Serial.print("X = "); Serial.print(GyX);
+  Serial.print(" | Y = "); Serial.print(GyY);
+  Serial.print(" | Z = "); Serial.println(GyZ);
+  Serial.println(" ");
+  delay(333);
+  
+}
